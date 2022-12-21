612a0339219a7c356bf498e2933a69714ea12396
Sean205429 <111170481+Sean205429@users.noreply.github.com>
2022-12-21 16:01:54 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "service-worker-demo";
+const urlsToCache = ["/", "/js/gsap.js", "/js/MorphSVGPlugin3.js", "/js/DrawSVGP.js", "/js/MotionPath.js", "/js/Physics2D.js", "/js/EasePack.js"];
+
+this.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("[Service Worker]", urlsToCache);
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+this.addEventListener("activate", event => {
+    // 不在白名单的`CACHE_NAME`就清理
+    const cacheWhitelist = ["service-worker-demo"];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    // 查看一下缓存
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.keys().then(res => console.log(res)))
+    );
+});
+
+this.addEventListener("fetch", event => {
+    const url = new URL(event.request.url);
+    if (url.origin === location.origin && urlsToCache.indexOf(url.pathname) > -1) {
+        event.respondWith(
+            caches.match(event.request).then(resp => {
+                if (resp) {
+                    console.log("fetch ", event.request.url, "有缓存，从缓存中取");
+                    return resp;
+                } else {
+                    console.log("fetch ", event.request.url, "没有缓存，网络获取");
+                    // // 如果有需要的话我们不需要在事件响应时进行匹配 可以直接将所有发起过的请求缓存
+                    return fetch(event.request).then(response => {
+                        return caches.open(CACHE_NAME).then(cache => {
+                           cache.put(event.request, response.clone());
+                            return response;
+                        });
+                    });
+                }
+            })
+        );
+    }
+});
